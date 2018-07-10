5b29de1e6208a18464966fbcc6413d4a926ce048
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-07-10 08:43:22 +0700

    Categories Index

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -39,7 +39,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('kategori.index') }}" class="nav-link">
+                            <a href="{{ route('categories.index') }}" class="nav-link">
                                 <i class="fa fa-circle-o nav-icon"></i>
                                 <p>Kategori</p>
                             </a>
