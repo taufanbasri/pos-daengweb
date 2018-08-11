0c548d9dbd94cc9ce675233aed74f205f43f6873
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-08-11 12:30:57 +0700

    Home edit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -14,7 +14,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
               <li class="breadcrumb-item"><a href="{{ route('products.index') }}">Produk</a></li>
               <li class="breadcrumb-item active">Tambah</li>
             </ol>
