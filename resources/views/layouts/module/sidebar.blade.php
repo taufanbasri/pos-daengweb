d5cdbe395a94086225ec88f8633d20e443f2e494
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-08-13 07:42:05 +0700

    Middleware permission

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -26,29 +26,31 @@
             </p>
           </a>
         </li>
-        <li class="nav-item has-treeview">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fa fa-server"></i>
-            <p>
-              Manajemen Produk
-              <i class="right fa fa-angle-left"></i>
-            </p>
-          </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="{{ route('categories.index') }}" class="nav-link">
-                <i class="fa fa-circle-o nav-icon"></i>
-                <p>Kategori</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ route('products.index') }}" class="nav-link">
-                <i class="fa fa-circle-o nav-icon"></i>
-                <p>Produk</p>
-              </a>
-            </li>
-          </ul>
-        </li>
+        @if (auth()->user()->can('show products') || auth()->user()->can('create products') || auth()->user()->can('delete products'))
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-server"></i>
+              <p>
+                Manajemen Produk
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('categories.index') }}" class="nav-link">
+                  <i class="fa fa-circle-o nav-icon"></i>
+                  <p>Kategori</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('products.index') }}" class="nav-link">
+                  <i class="fa fa-circle-o nav-icon"></i>
+                  <p>Produk</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        @endif
         <li class="nav-item has-treeview">
           <a href="#" class="nav-link">
             <i class="nav-icon fa fa-user"></i>
--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,28 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function()
 {
+    Route::group(['middleware' => ['role:admin']], function()
+    {
+        Route::resource('roles', 'RoleController')->only('index', 'store', 'destroy');
+        Route::resource('users', 'UserController')->except('show');
+        Route::get('users/{user}/roles', 'UserController@role')->name('users.roles');
+        Route::put('users/{user}/roles', 'UserController@setRole')->name('users.set-role');
+        Route::get('users/role-permissions', 'UserController@rolePermission')->name('users.roles-permission');
+        Route::post('users/permissions', 'UserController@addPermission')->name('users.add-permission');
+        Route::put('users/permissions/{role}', 'UserController@setRolePermission')->name('users.set-role-permission');
+    });
+
+    Route::group(['middleware' => ['permission:show products|create products|delete products']], function()
+    {
+        Route::resource('categories', 'CategoryController')->except('create', 'show');
+        Route::resource('products', 'ProductController')->except('show');
+    });
+
+    Route::group(['middleware' => ['role:kasir']], function()
+    {
+        Route::resource('categories', 'CategoryController')->except('create', 'show');
+        Route::resource('products', 'ProductController')->except('show');
+    });
+
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::resource('categories', 'CategoryController')->except('create', 'show');
-    Route::resource('products', 'ProductController')->except('show');
-    Route::resource('roles', 'RoleController')->only('index', 'store', 'destroy');
-    Route::resource('users', 'UserController')->except('show');
-    Route::get('users/{user}/roles', 'UserController@role')->name('users.roles');
-    Route::put('users/{user}/roles', 'UserController@setRole')->name('users.set-role');
-    Route::get('users/role-permissions', 'UserController@rolePermission')->name('users.roles-permission');
-    Route::post('users/permissions', 'UserController@addPermission')->name('users.add-permission');
-    Route::put('users/permissions/{role}', 'UserController@setRolePermission')->name('users.set-role-permission');
 });
